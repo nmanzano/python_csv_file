c4f130563d6cc3592a613bda94cea3e0235c62b6
Noni <noni.manzano@gmail.com>
2018-05-06 17:02:14 -0500

    solved

--- index.py
+++ index.py
@@ -1,50 +1,79 @@
-# !/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Task 01 module"""
-
+"""Task 01, 02, 03"""
 import csv
+import json
 
-grades = {
+
+grade_chart = {
     'A': float(1),
     'B': float(.9),
     'C': float(.8),
     'D': float(.7),
     'F': float(.6),
-    }
+}
 
-def get_score_summary(string):
+def get_score_summary(passfile):
+
+    fhandler = open(passfile, 'r')
+    readscore = csv.reader(fhandler, delimiter=',')
     simple_dict = {}
-    num_resturants = {}
-    final_score = {}
 
-    csvfile = open('inspection_results.csv', 'r')
-    reader = csv.DictReader(csvfile, delimiter=',')
+    for row in readscore:
+        boro = row[1]
+        grade = row[10]
+        camisid = row[0]
 
-    for row in reader:
-        camisid = row['CAMIS']
-        grade = row['GRADE']
-        boro = row['BORO']
-
-
-        if camisid not in simple_dict and grade in grades:
+        if camisid not in simple_dict and grade in grade_chart:
             simple_dict[camisid] = [grade, boro]
+    fhandler.close()
 
-    csvfile.close()
+    deduplicated = {}
+    for grade in simple_dict.values():
+        letter = grade[0]
+        borough = grade[1]
 
-    for resturants in simple_dict.values():
-
-        boro = resturants[1]
-        grade = resturants[0]
-
-        if boro in num_resturants:
-            num_resturants[boro][1] += grades[grade]
-            num_resturants[boro][0] += 1
+        if borough in deduplicated:
+            deduplicated[borough][1] += grade_chart[letter]
+            deduplicated[borough][0] += 1
         else:
-            num_resturants[boro] = [1, grades[grade]]
+            deduplicated[borough] = [1, grade_chart[letter]]
 
-    for boro, grade in num_resturants.iteritems():
+    final_score = {}
+    for boro, grade in deduplicated.iteritems():
         final_score[boro] = grade[0], grade[1]/grade[0]
-
     return final_score
 
-get_score_summary('inspection_results.csv')
+
+def get_market_density(passfile):
+
+    fhandler = open(passfile, 'r')
+    marketload = json.load(fhandler)
+    mdata = {}
+
+    for values in marketload['data']:
+        boro = values[8].strip()
+
+        if boro in mdata:
+            mdata[boro] += 1
+        else:
+            mdata[boro] = 1
+    return mdata
+
+
+def correlate_data(restaurants, markets, outfile):
+
+    fhandler = open(outfile, 'w')
+    restaurants = get_score_summary(restaurants)
+    markets = get_market_density(markets)
+    final_result = {}
+
+    for key, value in markets.iteritems():
+        boro = key.upper()
+        num_markets = float(value)
+        num_resturants = float(restaurants[boro][0])
+
+        if boro in restaurants:
+            final_result[boro] = restaurants[boro][1], num_markets/num_resturants
+
+    json.dump(final_result, fhandler)
